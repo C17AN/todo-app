bece313e01a5cf32a15dc5b1b3ff7604584015c3
김찬민 <chanmin@gimchanmin-ui-MacBookPro.local>
2023-06-03 00:52:42 +0900

    chore: AddTodo 모달 인터렉션을 추가한다

--- frontend/src/components/pages/home/TodoList/AddTodoModal.tsx
+++ frontend/src/components/pages/home/TodoList/AddTodoModal.tsx
@@ -1,54 +1,129 @@
 import Modal from "@/components/common/Modal";
 import styled from "@emotion/styled";
 import colors from "material-colors";
-import { ComponentProps } from "react";
-import { Variants, motion } from "framer-motion";
+import { ComponentProps, useState } from "react";
+import { AnimatePresence, Variants, motion } from "framer-motion";
+import { TodoCategory as TodoCategoryType } from "@/models/Todo";
+import Input from "@/components/common/Input";
+import Button from "@/components/common/Button";
 
 type Props = {} & ComponentProps<typeof Modal>;
 
+// Note: Animation Variant 역시 전역으로 관리하는게 나을지 고민해보기
 const variant: Variants = {
   hover: {
     scale: 0.95,
     color: colors.grey[800],
   },
+  invisible: {
+    opacity: 0,
+  },
+  fadeOut: {
+    x: "-100%",
+    transition: {
+      duration: 0.3,
+    },
+    opacity: 0,
+  },
+  fadeIn: {
+    transition: {
+      delay: 0.3,
+    },
+    opacity: 1,
+  },
 };
 
+type AddTodoStep = "카테고리" | "상세정보";
+
 const AddTodoModal = ({ open, onClose }: Props) => {
+  const [selectedCategory, setSelectedCategory] =
+    useState<TodoCategoryType | null>(null);
+  const [addTodoStep, setAddTodoStep] = useState<AddTodoStep>("카테고리");
+
+  const handleClose = () => {
+    setSelectedCategory(() => null);
+    setAddTodoStep(() => "카테고리");
+    onClose();
+  };
+
+  const selectCategory = (category: TodoCategoryType) => {
+    setSelectedCategory(() => category);
+    setAddTodoStep(() => "상세정보");
+  };
+
   return (
-    <Modal open={open} onClose={onClose} title="새로운 일정을 등록해주세요">
-      <TodoCategoryContainer>
-        <TodoCategory variants={variant} whileHover={"hover"}>
-          <img
-            src="https://raw.githubusercontent.com/toss/tossface/main/dist/svg/u1F3E0.svg"
-            alt="생활"
-          />
-          생활
-        </TodoCategory>
-        <TodoCategory variants={variant} whileHover={"hover"}>
-          <img
-            src="https://raw.githubusercontent.com/toss/tossface/main/dist/svg/u1F4D6.svg"
-            alt="학습"
-          />
-          학습
-        </TodoCategory>
-        {/* <TodoCategory variants={variant} whileHover={"hover"}>
-          <img src="/icons/icon_development.png" alt="개발" />
-          개발
-        </TodoCategory> */}
-        <TodoCategory variants={variant} whileHover={"hover"}>
-          <img
-            src="https://raw.githubusercontent.com/toss/tossface/main/dist/svg/u1F3AE.svg"
-            alt="취미"
-          />
-          취미
-        </TodoCategory>
-      </TodoCategoryContainer>
-      <div></div>
+    <Modal open={open} onClose={handleClose} title="새로운 일정을 등록해주세요">
+      <ContentContainer>
+        <AnimatePresence>
+          {addTodoStep === "카테고리" && (
+            <TodoCategoryContainer
+              variants={variant}
+              animate="fadeIn"
+              exit="fadeOut"
+            >
+              <TodoCategory
+                variants={variant}
+                whileHover={"hover"}
+                onClick={() => selectCategory("생활")}
+              >
+                <img
+                  src="https://raw.githubusercontent.com/toss/tossface/main/dist/svg/u1F3E0.svg"
+                  alt="생활"
+                />
+                생활
+              </TodoCategory>
+              <TodoCategory
+                variants={variant}
+                whileHover={"hover"}
+                onClick={() => selectCategory("학습")}
+              >
+                <img
+                  src="https://raw.githubusercontent.com/toss/tossface/main/dist/svg/u1F4D6.svg"
+                  alt="학습"
+                />
+                학습
+              </TodoCategory>
+              <TodoCategory
+                variants={variant}
+                whileHover={"hover"}
+                onClick={() => selectCategory("취미")}
+              >
+                <img
+                  src="https://raw.githubusercontent.com/toss/tossface/main/dist/svg/u1F3AE.svg"
+                  alt="취미"
+                />
+                취미
+              </TodoCategory>
+            </TodoCategoryContainer>
+          )}
+        </AnimatePresence>
+        <AnimatePresence>
+          {addTodoStep === "상세정보" && (
+            <TodoInformationInputContainer
+              variants={variant}
+              initial="invisible"
+              animate="fadeIn"
+            >
+              <InputContainer>
+                <Input placeholder="제목을 입력해주세요" />
+                <Input placeholder="내용을 입력해주세요" />
+              </InputContainer>
+              <Button size="cta" onClick={() => {}}>
+                등록하기
+              </Button>
+            </TodoInformationInputContainer>
+          )}
+        </AnimatePresence>
+      </ContentContainer>
     </Modal>
   );
 };
 
-const TodoCategoryContainer = styled.ul`
+const ContentContainer = styled.div`
+  overflow: hidden;
+`;
+
+const TodoCategoryContainer = styled(motion.ul)`
   display: flex;
   flex-direction: column;
   gap: 8px;
@@ -73,6 +148,13 @@ const TodoCategory = styled(motion.li)`
     height: 40px;
     margin-bottom: 8px;
   }
+`;
+
+const TodoInformationInputContainer = styled(motion.div)``;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
 export default AddTodoModal;
--- frontend/src/models/Todo.ts
+++ frontend/src/models/Todo.ts
@@ -1,5 +1,8 @@
+export type TodoCategory = "생활" | "취미" | "학습";
+
 export type Todo = {
   title: string;
   description: string;
   priority: number;
+  category: TodoCategory;
 };
