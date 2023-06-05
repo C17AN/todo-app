b8ea149cbe873a2ef0eb35eaadc1eb411dbd713d
chanmin <skp.5004037@sk.com>
2023-06-05 17:00:37 +0900

    chore: 바텀 네비게이션 바 스타일을 적용한다

--- frontend/src/components/common/BottomNavigation/index.tsx
+++ frontend/src/components/common/BottomNavigation/index.tsx
@@ -1,6 +1,10 @@
 import style from "./BottomNavigation.module.scss";
 import className from "classnames/bind";
 import { Link } from "react-router-dom";
+import { HiOutlineHome } from "@react-icons/all-files/hi/HiOutlineHome";
+import { HiOutlineCalendar } from "@react-icons/all-files/hi/HiOutlineCalendar";
+import { HiOutlineBookmark } from "@react-icons/all-files/hi/HiOutlineBookmark";
+import { HiOutlineCog } from "@react-icons/all-files/hi/HiOutlineCog";
 
 const cx = className.bind(style);
 
@@ -10,16 +14,20 @@ const BottomNavigation = (props: Props) => {
   return (
     <div className={cx("bottom-navigation")}>
       <Link to="/" className={cx("bottom-navigation-menu")}>
-        <div>홈</div>
+        <HiOutlineHome />
+        <div className={cx("bottom-navigation-menu-text")}>홈</div>
       </Link>
       <Link to="/calendar" className={cx("bottom-navigation-menu")}>
-        <div>캘린더</div>
+        <HiOutlineCalendar />
+        <div className={cx("bottom-navigation-menu-text")}>캘린더</div>
       </Link>
       <Link to="/bookmark" className={cx("bottom-navigation-menu")}>
-        <div>북마크</div>
+        <HiOutlineBookmark />
+        <div className={cx("bottom-navigation-menu-text")}>북마크</div>
       </Link>
       <Link to="/profile" className={cx("bottom-navigation-menu")}>
-        <div>프로필</div>
+        <HiOutlineCog />
+        <div className={cx("bottom-navigation-menu-text")}>프로필</div>
       </Link>
     </div>
   );
